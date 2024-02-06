8ba06f4f336c1a17544b0047670afadf8f342b81
Danny Nguyen <dqngyen1@memphis.edu>
2024-02-06 14:52:15 -0600

    Added code that allow user to quit and retry again if they guess wrong number until the answer is correct

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,19 @@
+
+
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
-
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
+while True:
+    guess = input("What number am I thinking of? (or type 'q' to quit) ")
+    if guess.lower() == "q":
+        print(f"The system is closed.\nThe number was {number}.")
+        break
+    try:
+        guess = int(guess)
+        if guess == number:
+            print("Congratulations, your guess is correct!")
+            break
+        else:
+            print("Invalid, try again.")
+    except ValueError:
+        print("Invalid. Please try again or type 'q' to quit.")
