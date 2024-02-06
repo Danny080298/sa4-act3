5950fd947d49792dcc0e5ab023e375ef7d644736
Danny Nguyen <dqngyen1@memphis.edu>
2024-02-06 15:16:15 -0600

    Add code that limit user's guess and tell them how many trials left after they input their guess

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,10 @@
 
 
 number = 10
-
+try_guess = 0
+limited = 5
 print("I'm thinking of a number...")
-while True:
+while try_guess != limited:
     guess = input("What number am I thinking of? (or type 'q' to quit) ")
     if guess.lower() == "q":
         print(f"The system is closed.\nThe number was {number}.")
@@ -14,6 +15,13 @@ while True:
             print("Congratulations, your guess is correct!")
             break
         else:
-            print("Invalid, try again.")
+            print(f"Invalid, try again, you have {limited- 1} trials lefts.")  
+            limited -= 1
+            if limited == 0:
+                print("You run out of trials")
+                print(f"The system is closed.\nThe number was {number}.")
+                break
     except ValueError:
         print("Invalid. Please try again or type 'q' to quit.")
+
+    try_guess += 1
